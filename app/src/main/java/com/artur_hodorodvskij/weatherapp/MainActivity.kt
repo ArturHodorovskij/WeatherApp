3b07d39a4b84bb4b7907d7ec08d8a7aa71f27a8f
ArturHodorovskij <tjarturtj@gmail.com>
2025-04-25 19:36:02 +0300

    рефач

---
+++ app/src/main/java/com/artur_hodorodvskij/weatherapp/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.artur_hodorodvskij.weatherapp
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+import com.artur_hodorodvskij.weatherapp.ui.theme.WeatherAppTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            WeatherAppTheme {
+
+            }
+        }
+    }
+}
+
+
+
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+
+}
---
+++ app/src/main/java/com/artur_hodorodvskij/weatherapp/presentation/screen/MainScreen.kt
@@ -0,0 +1,2 @@
+package com.artur_hodorodvskij.weatherapp.presentation.screen
+
